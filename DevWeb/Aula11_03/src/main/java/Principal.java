6beaaeab168a38b96c90185ccff0871260d425c0
Amanda Lemos Ribas <amandaribas@id.uff.br>
2026-03-11 17:23:51 -0300

    .

---
+++ DevWeb/Aula11_03/src/main/java/Principal.java
@@ -0,0 +1,9 @@
+import corejava.Console;
+
+public class Principal {
+    public static void main(String[] args){
+        double preco = Console.readDouble("Valor:");
+        System.out.println(preco);
+    }
+
+}
